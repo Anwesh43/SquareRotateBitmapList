f207633336b77780f0ba623239147023ca4c749b
Anwesh43 <anweshthecool0@gmail.com>
2017-05-19 11:43:31 +0530

    Using RotateBitmapViewList in your app

--- app/src/main/java/com/anwesome/ui/rotatebitmapviewdemo/MainActivity.java
+++ app/src/main/java/com/anwesome/ui/rotatebitmapviewdemo/MainActivity.java
@@ -1,19 +1,25 @@
 package com.anwesome.ui.rotatebitmapviewdemo;
 
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ViewGroup;
 
 import com.anwesome.ui.rotatebitmapview.RotateBitmapView;
+import com.anwesome.ui.rotatebitmapview.RotateBitmapViewList;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        RotateBitmapView rotateBitmapView = new RotateBitmapView(this, BitmapFactory.decodeResource(getResources(),R.drawable.stp));
-        addContentView(rotateBitmapView,new ViewGroup.LayoutParams(600,600));
+        //setContentView(R.layout.activity_main);
+        RotateBitmapViewList rotateBitmapViewList = new RotateBitmapViewList(this);
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.stp);
+        for(int i=0;i<5;i++) {
+            rotateBitmapViewList.addImage(bitmap);
+        }
+        rotateBitmapViewList.show();
     }
 }
