c9c641691727e3bace7c82bd3ef347df0b9caa1e
Anwesh43 <anweshthecool0@gmail.com>
2017-05-19 11:10:41 +0530

    Adding ListLayout

---
+++ rotatebitmapview/src/main/java/com/anwesome/ui/rotatebitmapview/ListLayout.java
@@ -0,0 +1,52 @@
+package com.anwesome.ui.rotatebitmapview;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Point;
+import android.hardware.display.DisplayManager;
+import android.view.Display;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by anweshmishra on 19/05/17.
+ */
+public class ListLayout extends ViewGroup{
+    private int time = 0,w,h;
+    public ListLayout(Context context){
+        super(context);
+        initDimension(context);
+    }
+    public void initDimension(Context context) {
+        DisplayManager displayManager = (DisplayManager)context.getSystemService(Context.DISPLAY_SERVICE);
+        Display display = displayManager.getDisplay(0);
+        if(display != null) {
+            Point size = new Point();
+            display.getRealSize(size);
+            w = size.x;
+            h = size.y;
+        }
+    }
+    public void addImage(Bitmap bitmap) {
+        RotateBitmapView rotateBitmapView = new RotateBitmapView(getContext(),bitmap);
+        addView(rotateBitmapView,new LayoutParams(9*w/10,9*w/10));
+        requestLayout();
+    }
+    public void onMeasure(int wspec,int hspec) {
+        int newH = h/40;
+        for(int i=0;i<getChildCount();i++) {
+            View child = getChildAt(i);
+            measureChild(child,wspec,hspec);
+            newH += child.getMeasuredWidth() + h/40;
+        }
+        setMeasuredDimension(w,Math.max(newH,h));
+    }
+    public void onLayout(boolean reloaded,int a,int b,int w,int h) {
+        int x = w/20,y = w/40;
+        for(int i=0;i<getChildCount();i++) {
+            View child = getChildAt(i);
+            child.layout(x,y,x+9*w/10,y);
+            y += 9*w/10;
+        }
+    }
+}
