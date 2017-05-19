c5919474e61a0044979926018b6af20866be3b74
Anwesh43 <anweshthecool0@gmail.com>
2017-05-19 11:16:52 +0530

    Adding List class

--- rotatebitmapview/src/main/java/com/anwesome/ui/rotatebitmapview/RotateBitmapViewList.java
+++ rotatebitmapview/src/main/java/com/anwesome/ui/rotatebitmapview/RotateBitmapViewList.java
@@ -2,6 +2,7 @@ package com.anwesome.ui.rotatebitmapview;
 
 import android.app.Activity;
 import android.graphics.Bitmap;
+import android.view.ViewGroup;
 import android.widget.ScrollView;
 
 /**
@@ -10,18 +11,22 @@ import android.widget.ScrollView;
 public class RotateBitmapViewList {
     private Activity activity;
     private boolean isShown = false;
+    private ListLayout listLayout;
     private ScrollView scrollView;
     public RotateBitmapViewList(Activity activity) {
         this.activity = activity;
+        listLayout = new ListLayout(activity);
         scrollView = new ScrollView(activity);
+        scrollView.addView(listLayout,new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
     }
     public void addImage(Bitmap bitmap) {
         if(!isShown) {
-
+            listLayout.addImage(bitmap);
         }
     }
     public void show() {
         if(!isShown) {
+            activity.setContentView(scrollView);
             isShown = true;
         }
     }
